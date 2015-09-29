35a5a2348c2fbaebc1e0a40a02fc5f2c3416fbf6
reinhold <reinhold@web.de>
2015-09-29 11:38:14 +0200

    Version1

---
+++ Model/Message.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace Model
+{
+    public class Message:INotifyPropertyChanged
+    {
+        private String text;
+
+        public String Text
+        {
+            get { return text; }
+            set 
+            { 
+                text = value;
+                onPropertyChanged("Text");
+            }
+        }
+        public Message()
+        {
+            text = "Fi12";
+        }
+
+        #region
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void onPropertyChanged(String propertyName)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+
+    }
+}
---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows;
+using Model;
+
+namespace ViewModel
+{
+    public class MainViewModel
+    {
+        private ICommand buttonCommand;       
+        public ICommand ButtonCommand
+        {
+            get { return buttonCommand; }
+            set { buttonCommand = value; }
+        }
+        private ICommand buttonCommand2;
+        public ICommand ButtonCommand2 
+        {
+            get { return buttonCommand2; }
+            set { buttonCommand2 = value; }
+        }
+        private Message text;
+
+        public Message Text
+        {
+            get { return text; }
+            set { text = value; }
+        }
+        
+   
+     
+        public MainViewModel()
+        {
+            this.Text = new Message();
+            this.ButtonCommand = new UserCommand(new  Action<Object> (ShowMessage));
+            this.ButtonCommand2 = new UserCommand(new Action<Object>(ChangeMessage));
+            
+        }
+        private void ShowMessage(Object message)
+        {
+            MessageBox.Show(this.Text.Text);
+        }
+        private void ChangeMessage(Object message)
+        {
+            Text.Text = (String) message;
+        }
+    }
+}
